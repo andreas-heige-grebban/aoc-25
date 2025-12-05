9db3daf1524235e7a31d4058e1d389413751f5de
Heige Andreas <andreas.heige@grebban.com>
2025-12-05 13:49:16 +0100

    build: mono setup

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'no-console': 'off',
+      'no-eval': 'off',
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**'],
+  },
+];
